ca547c94baf5321c836322ef221b97e3c748c7a9
Katarina Tuttle <katka.karolcikova@gmail.com>
2016-07-01 07:50:35 -0700

    Create file app.rb

---
+++ app.rb
@@ -0,0 +1,17 @@
+require('sinatra')
+require('sinatra/reloader')
+require('pry')
+require('./lib/anagrams')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get('/results') do
+  @input = params.fetch('input')
+  @sentence = params.fetch('sentence')
+  @results = @input.word_count(@sentence)
+  erb(:results)
+
+end
